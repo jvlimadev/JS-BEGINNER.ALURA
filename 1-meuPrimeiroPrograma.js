c5e2314531cbad4552f54d9988fa66bf579daac4
jvlimadev <jvlima_dev1999@outlok.com>
2023-03-04 15:42:37 -0300

    Adicionando variáveis e aplicando operações aritiméticas

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,15 @@
-console.log("Meu primeiro prorgrama");
+console.log("Meu primeiro prorgrama: trabalhando com variáveis. ");
+
+const idade  = 23;
+
+console.log(idade);
+
+const idadeSomada = idade + 2;
+const idadeSubtraida = idade - 2;
+const idadeMultiplicada = idade * 2;
+const idadeDividida = idade / 2;
+
+console.log(idadeSomada);
+console.log(idadeSubtraida);
+console.log(idadeMultiplicada);
+console.log(idadeDividida);
